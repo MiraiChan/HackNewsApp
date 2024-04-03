3138f97bff416122e2d34ade68962f77c0e71d47
Almira Khafizova <mimirka80@gmail.com>
2024-04-03 11:51:05 +0200

    Initial Commit

---
+++ HackNewsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackNewsApp
+//
+//  Created by Almira Khafizova on 03.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackNewsApp/HackNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackNewsAppApp.swift
+//  HackNewsApp
+//
+//  Created by Almira Khafizova on 03.04.24.
+//
+
+import SwiftUI
+
+@main
+struct HackNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
