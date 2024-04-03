16fdd0304c9bb0e2c8f8170fe133e1e37e8f7978
Almira Khafizova <mimirka80@gmail.com>
2024-04-03 12:49:30 +0200

    Implement NavigationView, List and URL to display HackNewsApp posts.

--- HackNewsApp/ContentView.swift
+++ HackNewsApp/ContentView.swift
@@ -9,16 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+            }
+            .navigationTitle("HackNewsApp")
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "List of News")
+]
---
+++ HackNewsApp/Models/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  HackNewsApp
+//
+//  Created by Almira Khafizova on 03.04.24.
+//
+
+import Foundation
+
+class NetworkManager {
+    func fetchData() {
+        let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page")
+    }
+}
